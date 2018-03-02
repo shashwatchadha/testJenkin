85a89e8de2d0321772ec6a5df20589fffc8f1652
shashwatchadha <shashwatchadhaa@gmail.com>
2018-03-02 20:49:55 +0530

    dfgdfg

---
+++ src/test/java/myTest.java
@@ -0,0 +1,32 @@
+import org.testng.annotations.Test;
+
+public class myTest {
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@Test
+	public void testCase()
+	{
+		
+		System.out.println("testing 1");
+	}
+	
+	
+	
+	
+	
+	
+	
+}
